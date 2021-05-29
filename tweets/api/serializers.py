7174c06149d4073b699d053d15870c5d54a74c81
Odette Du <odettedu@odettedembp.lan>
2021-05-29 10:30:17 -0700

    tweet api

---
+++ tweets/api/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers, exceptions
+from accounts.api.serializers import UserSerializer
+from tweets.models import Tweet
+
+class TweetSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
+    class Meta:
+        model = Tweet
+        fields = ('id', 'user', 'created_at', 'content')
+
+class TweetCreateSerializer(serializers.ModelSerializer):
+    content = serializers.CharField(max_length=140, min_length=6)
+
+    class Meta:
+        model = Tweet
+        fields = ('content', )
+
+    def create(self, validated_data):
+        user = self.context['request'].user
+        content = validated_data['content']
+        tweet = Tweet.objects.create(user=user, content = content)
+        return tweet
