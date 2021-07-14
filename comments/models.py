bd2b025d3781ac365d5429dbe75a76a0b8d0f7c6
Odette Du <odettedu@OdettedeMBP.fios-router.home>
2021-07-14 16:50:48 -0400

    like model

--- comments/models.py
+++ comments/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
 
 from tweets.models import Tweet
+from likes.models import Like
 
 
 class Comment(models.Model):
@@ -16,3 +18,10 @@ class Comment(models.Model):
 
     def __str__(self):
         return f'{self.created_at} - {self.user} says {self.content} at tweet {self.tweet}'
+
+    @property
+    def like_set(self):
+        return Like.objects.filter(
+            content_type=ContentType.objects.get_for_model(Comment),
+            object_id=self.id,
+        ).order_by('-created_at')
